40e4a80bd703f12cc9459714f1735d70ad59f901
vitaliyaL <vita0623@mail.ru>
2022-04-30 19:14:56 +0300

    feat: task2 added

--- script/app.js
+++ script/app.js
@@ -8,12 +8,11 @@ if (!isNaN(num1) && num1) {
       );
     }
     if (num1 > num2) {
-        for (let i = num1; i >= num2; i--)
-      {
+      for (let i = num1; i >= num2; i--) {
         console.log(i);
       }
     } else {
-      for (let i = num1; i <= num2; i++)  {
+      for (let i = num1; i <= num2; i++) {
         console.log(i);
       }
     }
@@ -22,4 +21,44 @@ if (!isNaN(num1) && num1) {
   }
 } else {
   console.log("Некорректный ввод.");
+}
+
+let i = 3;
+let j = 4;
+let pin = prompt("Введите PIN: у вас 3 попытки");
+
+pin ? password(i, j, pin) : alert("Всего доброго!");
+
+function password(i, j, pin) {
+  if (i > 1) {
+    if (pin === "12345") {
+      alert("Добро пожаловать!");
+    } else {
+      i--;
+      pin = prompt(`Введите PIN: \nКоличество оставшихся попыток: ${i}`);
+      if (pin) {
+        password(i, j, pin);
+      } else {
+        alert("Всего доброго!");
+      }
+    }
+  } else {
+    //puk
+    if (j > 0) {
+        if (pin === "67890") {
+            alert("Добро пожаловать!");
+          }
+        else{
+        j--;
+        pin = prompt(`Введите PUK: \nКоличество оставшихся попыток: ${j}`);
+        if (pin) {
+            password(i, j, pin);
+          } else {
+            alert("Всего доброго!");
+          }
+        }
+    } else {
+      alert("Всего доброго!");
+    }
+  }
 }
